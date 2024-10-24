b033d1ddcee44dedef34467810a6a36816a28a6e
Dahee Park <dahee5654@gmail.com>
2024-10-24 13:13:55 +0900

    adding Order Schema

--- Model/Order.js
+++ Model/Order.js
@@ -21,3 +21,15 @@ const orderSchema = Schema(
   },
   { timestamps: true }
 );
+
+orderSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+};
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
