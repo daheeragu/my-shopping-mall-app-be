997295c13638fed3f5ef4db978bfa5fd2085228e
Dahee Park <dahee5654@gmail.com>
2024-10-29 20:09:09 +0900

    shopping mall 12-13

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -34,4 +34,27 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+productController.getProducts = async (req, res) => {
+  try {
+    const { page, name } = req.query;
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(cond);
+
+    //쿼리를 실행
+    const productList = await query.exec();
+
+    // if (name) {
+    //   const products = await Product.find({
+    //     name: { $regex: name, $option: "i" },
+    //   });
+    // } else {
+    //   const products = await Product.find({});
+    // }
+
+    res.status(200).json({ status: "success", data: productList });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
 module.exports = productController;
