44b2cbb54d38018ecda052d2d055aaa6b4fc37ea
Dahee Park <dahee5654@gmail.com>
2024-10-30 20:46:32 +0900

    pagination add

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,6 @@
 const Product = require("../models/Product");
 const productController = {};
-
+const PAGE_SIZE = 4;
 productController.createProduct = async (req, res) => {
   try {
     const {
@@ -39,9 +39,22 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
+    let response = { status: "success" };
+
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+
+      // 최종 몇 개의 페이지가 필요로 한지
+      // 데이터가 총 몇개인지 확인
+      const totalItemNum = await Product.find(cond).countDocuments();
+      // 그 데이터 총 개수 / PAGE_SIZE
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     //쿼리를 실행
     const productList = await query.exec();
+    response.data = productList;
 
     // if (name) {
     //   const products = await Product.find({
@@ -51,7 +64,7 @@ productController.getProducts = async (req, res) => {
     //   const products = await Product.find({});
     // }
 
-    res.status(200).json({ status: "success", data: productList });
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
